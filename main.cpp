9d2428c34ebfa3ff0fe01092a838e3cfcdb1cbd6
Bran-ssj <00109425@uca.edu.sv>
2026-01-31 21:43:08 -0600

    add: insertar y estructura nodo y ptrs

--- main.cpp
+++ main.cpp
@@ -1,6 +1,58 @@
 #include <iostream>
 using namespace std;
 
+// Estructura del nodo
+struct Nodo {
+    int id;
+    string nombre;
+    float peso;
+    Nodo* sig;
+    Nodo* ant;
+};
+
+// Punteros globales
+Nodo* head = NULL;
+Nodo* tail = NULL;
+
+// Buscar por ID para validar repetidos
+Nodo* buscarPorId(int id) {
+    Nodo* aux = head;
+    while (aux) {
+        if (aux->id == id)
+            return aux;
+        aux = aux->sig;
+    }
+    return NULL;
+}
+
+void insertarInicio(int id, string nombre, float peso) {
+    if (buscarPorId(id)) return;
+
+    Nodo* nuevo = new Nodo{id, nombre, peso, NULL, NULL};
+
+    if (!head) {
+        head = tail = nuevo;
+    } else {
+        nuevo->sig = head;
+        head->ant = nuevo;
+        head = nuevo;
+    }
+}
+
+void insertarFinal(int id, string nombre, float peso) {
+    if (buscarPorId(id)) return;
+
+    Nodo* nuevo = new Nodo{id, nombre, peso, NULL, NULL};
+
+    if (!tail) {
+        head = tail = nuevo;
+    } else {
+        tail->sig = nuevo;
+        nuevo->ant = tail;
+        tail = nuevo;
+    }
+}
+
 int main() {
     return 0;
 }
